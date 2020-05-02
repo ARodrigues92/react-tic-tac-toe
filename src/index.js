1e81bb71e5300f3ba7ef38bd7f2f5d70cb2c1a23
André Rodrigues <arodrigues92@protonmail.ch>
2020-05-02 19:45:46 +0100

    Add 3rd improvement - create board with loops

--- src/index.js
+++ src/index.js
@@ -14,32 +14,36 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i.toString()}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
     );
   }
 
+  createBoard() {
+    let board = [];
+    let counter = 0;
+
+    for (let i = 0; i < 3; i++) {
+      let squares = [];
+
+      for (let j = 0; j < 3; j++) {
+        squares.push(this.renderSquare(counter));
+        counter += 1;
+      }
+
+      board.push(
+        <div key={i.toString()} className="board-row">
+          {squares}
+        </div>
+      );
+    }
+    return board;
+  }
+
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{this.createBoard()}</div>;
   }
 }
 
